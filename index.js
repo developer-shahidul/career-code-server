b8591a1a053b7af161a80b27b7b7284c487f2ba6
developer-shahidul <developer-shahidul@outlook.com>
2026-02-17 23:50:08 +0600

    email token varify

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 // middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "http://career-code-client-f17gue5rr-shaidul-portfollio.vercel.app",
+    ],
     credentials: true,
   }),
 );
@@ -21,11 +24,47 @@ app.use(cookieParser());
 // firebase jwt
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./firebase-admin-key.json");
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
+
+//firebase token verify
+const firebaseTokenVerify = async (req, res, next) => {
+  const authHeader = req.headers?.authorization;
+  console.log("token headers", authHeader);
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).send({ message: "unauthorize" });
+  }
+
+  const token = authHeader?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).send({ message: "unauthorize access" });
+  }
+
+  try {
+    const decoded = await admin.auth().verifyIdToken(token);
+    console.log("firebase token decoded", decoded);
+
+    req.decoded = decoded;
+    next();
+  } catch (err) {
+    res.status(401).send({ message: "unauthorize access" });
+  }
+};
 ///////////////////
 const logger = (req, res, next) => {
   // console.log("inside the logger middleware");
@@ -34,7 +73,7 @@ const logger = (req, res, next) => {
 
 const verifyToken = (req, res, next) => {
   const token = req?.cookies?.token;
-  // console.log("cookie in the middleware", token);
+  console.log("cookie in the middleware", token);
 
   if (!token) {
     return res.status(401).send({ message: "unauthorized access" });
@@ -50,35 +89,10 @@ const verifyToken = (req, res, next) => {
   });
 };
 
-//firebase token verify
-const firebaseTokenVerify = async (req, res, next) => {
-  const authHeader = req.headers?.authorization;
-
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(401).send({ message: "unauthorize" });
-  }
-
-  const token = authHeader?.split(" ")[1];
-
-  if (!token) {
-    return res.status(401).send({ message: "unauthorize access" });
-  }
-
-  try {
-    const decoded = await admin.auth().verifyIdToken(token);
-    // console.log("firebase token decoded", decoded);
-
-    req.decoded = decoded;
-    next();
-  } catch (err) {
-    res.status(401).send({ message: "unauthorize access" });
-  }
-};
-
 // verify token email check
 const verifyEmailToken = (req, res, next) => {
   if (req.query.email !== req.decoded.email) {
-    res.status(403).send({ message: "forbidden access" });
+    return res.status(403).send({ message: "forbidden access" });
   }
   next();
 };
@@ -275,10 +289,10 @@ app.patch("/applications/:id", async (req, res) => {
 
 app.get(
   "/applications/applicant",
-  logger,
-  firebaseTokenVerify,
+  // logger,
+  // firebaseTokenVerify,
   verifyToken,
-  // verifyEmailToken,
+  verifyEmailToken,
   async (req, res) => {
     try {
       const { applicationCollection, jobCollection } = await connectToDB();
